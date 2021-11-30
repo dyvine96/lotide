9d8b6ffd0a7483bf96ed3857edd7d51ca461dceb
Dymika Harte <dymika@unsgnd.ca>
2021-11-30 00:34:03 +0000

    converted headTest to use mocha and chai testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,16 @@
 // ../ goes back two directories. ./ goes back one
 // This file imports from assertEqual and Head
 
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head')
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Bye"); 
+
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1)
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
