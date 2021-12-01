5dd86a136e8f61fbf1b735ada91fd9714034ba4f
Dymika Harte <dymika@unsgnd.ca>
2021-12-01 00:16:38 +0000

    index file created

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+
+const head = require('./head');
+const tail = require('./tail');
+//const middle = require('./middle');
+const assertArraysEqual = require('/.assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual.js = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  //middle: middle
+};
