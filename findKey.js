e070bd2dacdef4165b38c9976569770b89965cbf
Dymika Harte <dymika@unsgnd.ca>
2021-12-05 16:11:57 +0000

    find key —— incomplete

---
+++ findKey.js
@@ -0,0 +1,7 @@
+// Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+const findKey = function(object,callback) {
+let foundKey = Object.keys(object).find(key => object[key] === value);
+}
+
+
